3eb8256b35ac6075be4acf2669639c2257d4642f
杨大锐 <1404933237@qq.com>
2020-07-23 18:39:29 +0800

    注册了登录页

---
+++ views/singlePage/login/login.vue
@@ -0,0 +1,35 @@
+<template>
+	<!-- 登录页 -->
+	<view class="s_login">
+		<view class="logo">
+			<image class="image" src="@/static/images/Product/logo.png" mode=""></image>
+		</view>
+		<view class="title-h">
+			您还未登录
+		</view>
+		<view class="message">
+			请登录后咋再进行操作
+		</view>
+		<button type="default"></button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				toUrl: ''
+			};
+		},
+		onLoad(option) {
+			if (option.url !== undefined || option.url === '') {
+				this.toUrl = option.url
+			}
+			return
+		}
+	}
+</script>
+
+<style lang="less">
+
+</style>
