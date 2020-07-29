7542e5707b169bcbb10ee2f28bd2ab5d1a749f66
陈铄源 <a821403526@163.com>
2020-07-29 12:21:00 +0800

    完成我的订单页面逻辑

--- views/singlePage/myOrder/myOrder.vue
+++ views/singlePage/myOrder/myOrder.vue
@@ -9,14 +9,18 @@
 			</navigator>
 			<u-tabs active-color='#000000' inactive-color="#000000" :list="list" :bold='true' :is-scroll="false" :current="current" @change="change" :bar-style="{backgroundImage: 'linear-gradient(to right,#92D0F9,#26A7FC)',height:6+'rpx'}"></u-tabs>
 		</view>
-		<view class="orderItem" v-for="(item,index) in 3" :key="index">
+		<view class="orderBg" v-if="currentArr.length=== 0">
+			<image src="../../../static/images/iconfont/dingdanBg.png"></image>
+			<view class="noOrder">还没有相关订单</view>
+		</view>
+		<view class="orderItem" v-for="item in currentArr" :key="item.id" v-else>
 			<view class="shopCon">
 				<view class="shopNameCon">
-					<view class="shopName">韩国年糕料理（海岸城店）</view>
+					<view class="shopName">{{item.shopName}}</view>
 					<image src="../../../static/images/iconfont/more.png"></image>
 
 				</view>
-				<view class="text">待付款</view>
+				<view class="text">{{item.orderType}}</view>
 			</view>
 			<view class="orderNews">
 				<view class="orderInformation">
@@ -25,19 +29,18 @@
 					</view>
 					<view class="information">
 						<view class="price">
-							<view class="priceText">100元代金券</view>
-							<view class="priceNumber">￥79.90</view>
+							<view class="priceText">{{item.priceText}}</view>
+							<view class="priceNumber">￥{{item.priceNumber}}</view>
 						</view>
-						<view class="orderTime">下单时间：2020-09-09 12:30</view>
-						<view class="orderNumber">数量：1</view>
+						<view class="orderTime">下单时间：{{item.orderTime}}</view>
+						<view class="orderNumber">数量：{{item.orderNumber}}</view>
 					</view>
 				</view>
-				<view class="toPlay">
-					<navigator class="playBtn" :url="orderDetail">去付款</navigator>
+				<view class="toPay">
+					<navigator class="payBtnOne" :url="orderDetail" v-if="item.orderType==='待付款'">去付款</navigator>
+					<navigator class="payBtnTwo" :url="timeKillProductDetail" v-else-if="item.orderType!=='待使用'">再来一单</navigator>
 				</view>
 			</view>
-
-
 		</view>
 
 	</view>
@@ -56,8 +59,47 @@
 				}, {
 					name: '已完成'
 				}],
+				orderList:[
+					{
+					shopName:'韩国年糕料理（海岸城店）',
+					priceText:'100元代金券',
+					priceNumber:'79.90',
+					orderTime:'2020-09-09 12:30',
+					orderNumber:'1',
+					orderType:'待付款'
+				},{
+					shopName:'韩国年糕料理（海岸城店）',
+					priceText:'100元代金券',
+					priceNumber:'79.90',
+					orderTime:'2020-09-09 12:30',
+					orderNumber:'1',
+					orderType:'待使用'
+				},{
+					shopName:'韩国年糕料理（海岸城店）',
+					priceText:'100元代金券',
+					priceNumber:'79.90',
+					orderTime:'2020-09-09 12:30',
+					orderNumber:'1',
+					orderType:'已完成'
+				},{
+					shopName:'韩国年糕料理（海岸城店）',
+					priceText:'100元代金券',
+					priceNumber:'79.90',
+					orderTime:'2020-09-09 12:30',
+					orderNumber:'1',
+					orderType:'待付款'
+				},{
+					shopName:'韩国年糕料理（海岸城店）',
+					priceText:'100元代金券',
+					priceNumber:'79.90',
+					orderTime:'2020-09-09 12:30',
+					orderNumber:'1',
+					orderType:'已退款'
+				}
+				],
 				current: 0,
 				orderDetail:'../../singlePage/orderDetail/orderDetail',  //跳转到待付款订单详情 
+				timeKillProductDetail:'../../singlePage/timeKillProductDetail/timeKillProductDetail',   //跳转到商品详情页面
 				toSearch:'../../singlePage/search/search'   // 跳转到搜索页面
 				
 			}
@@ -67,7 +109,33 @@
 				this.current = index;
 				console.log(this.current)
 			}
+		},
+		computed:{
+			currentArr(){
+				switch(this.current){
+					case 1:
+						const result1 = this.orderList.filter(item=>item.orderType==='待付款')
+						return result1
+					case 2:
+						const result2 = this.orderList.filter(item=>item.orderType==='待使用')
+						return result2
+					case 3:
+						const result3 = this.orderList.filter(item=>item.orderType==='已完成')
+						return result3
+					default:
+						return this.orderList
+				}
+			}
 		}
+		// onLoad(){
+		// 	uni.request({
+		// 		method:'GET',
+		// 		url:'xxx',
+		// 		success(res) {
+		// 			this.orderList = res.data
+		// 		}
+		// 	})
+		// }
 	}
 </script>
 
@@ -111,7 +179,25 @@
 			}
 
 		}
-
+		
+		.orderBg{
+			width: 370.5rpx;
+			height: 420.02rpx;
+			margin: 138rpx 190rpx 0 190rpx;
+			image{
+				width: 370.5rpx;
+				height: 420.02rpx;
+				margin-bottom: 42rpx;
+			}
+			.noOrder{
+				font-size:28rpx;
+				font-family:PingFang SC;
+				font-weight:400;
+				line-height:32rpx;
+				color:rgba(169,169,169,1);
+				text-align: center;
+			}
+		}
 		.orderItem {
 			width: 750rpx;
 			height: 348rpx;
@@ -226,12 +312,22 @@
 					}
 				}
 
-				.toPlay {
+				.toPay {
 					display: flex;
 					justify-content: flex-end;
 
-					.playBtn {
+					.payBtnOne {
 						width: 108rpx;
+						height: 54rpx;
+						border: 2rpx solid rgba(36, 167, 255, 1);
+						opacity: 1;
+						border-radius: 8rpx;
+						line-height: 54rpx;
+						text-align: center;
+						color:rgba(36,167,255,1);
+					}
+					.payBtnTwo{
+						width: 164rpx;
 						height: 54rpx;
 						border: 2rpx solid rgba(36, 167, 255, 1);
 						opacity: 1;
