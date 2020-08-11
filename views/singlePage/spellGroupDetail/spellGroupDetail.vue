3be9b04e36f0b8e7be2d3cbf2536a051203c44ab
陈铄源 <a821403526@163.com>
2020-08-11 18:08:36 +0800

    我的拼团分享待完善

--- views/singlePage/spellGroupDetail/spellGroupDetail.vue
+++ views/singlePage/spellGroupDetail/spellGroupDetail.vue
@@ -22,7 +22,7 @@
 			</view>
 			<!-- 拼团成功 -->
 			<view class="groupSuccess" v-else>拼团成功！</view>
-			<!-- 用户个人信息 -->	
+			<!-- 用户个人信息 -->
 			<view class="groupUser">
 				<view class="oneGroupUser" v-for="(item,index) in userList" :key="index">
 					<view class="userImg">
@@ -31,9 +31,9 @@
 					<view class="userName">{{item.userName}}</view>
 				</view>
 			</view>
-			<!-- 邀请好友拼团 -->	
+			<!-- 邀请好友拼团 -->
 			<view class="inviteFriends" v-if="type === '已支付，待成团' ">
-				<view class="inviteBtn" @click="inviteBtn">邀请好友拼团</view>
+				<button class="inviteBtn" @click="inviteBtn" open-type="share">邀请好友拼团</button>
 			</view>
 			<!-- 查看我的拼团 -->
 			<view class="inviteFriends" v-else @click="checkMySpellGroup">
@@ -64,43 +64,57 @@
 
 <script>
 	import oneShopDetail from '../../../src/publicComponents/oneShopDetail.vue'
-	export default{
-		data(){
-			return{
-				timestamp:'86400',   // 倒计时时间
-				num:'1',  //还差几人
-				userList:[{          // 用户列表
-					withInviteImg:'../../../static/images/tabBarImage/myLoginHeader.png',  //带邀请用户头像
-					userName:'上官婉儿',  // 用户名
-				},{
-					withInviteImg:'../../../static/images/tabBarImage/myLoginHeader.png',  //带邀请用户头像
-					userName:'鲁班大师',  // 用户名
-				},{
-					withInviteImg:'../../../static/images/iconfont/weizhitouxiang.png',  //带邀请用户头像
-					userName:'雷霆嘎巴',  // 用户名
-				}], 
-				groupType:'拼团失败',  //拼团状态
-				orderNumber:'293247384734',   // 订单编号
-				orderPrice:'66.66',     //订单金额
-				payWay:'支付宝',// 支付方式
-				type:''    //从我的拼团传过来的字段
+	export default {
+		data() {
+			return {
+				timestamp: '86400', // 倒计时时间
+				num: '1', //还差几人
+				userList: [{ // 用户列表		
+					withInviteImg: '../../../static/images/tabBarImage/myLoginHeader.png', //带邀请用户头像
+					userName: '上官婉儿', // 用户名
+				}, {
+					withInviteImg: '../../../static/images/tabBarImage/myLoginHeader.png', //带邀请用户头像
+					userName: '鲁班大师', // 用户名
+				}, {
+					withInviteImg: '../../../static/images/iconfont/weizhitouxiang.png', //带邀请用户头像
+					userName: '待邀请...', // 用户名
+				}],
+				groupType: '拼团失败', //拼团状态
+				orderNumber: '293247384734', // 订单编号
+				orderPrice: '66.66', //订单金额
+				payWay: '支付宝', // 支付方式
+				type: '' //从我的拼团传过来的字段
 			}
 		},
-		methods:{
-			checkMySpellGroup(){
+		methods: {
+			checkMySpellGroup() {
 				uni.navigateBack({
-					url:'/views/singlePage/mySpellGroup/mySpellGroup'
+					url: '/views/singlePage/mySpellGroup/mySpellGroup'
 				})
 			},
-			inviteBtn(){
+			inviteBtn() {
 				console.log('邀请好友')
+				uni.share({
+				    provider: "weixin",
+				    scene: "WXSceneSession",
+				    type: 5,
+				    title: "一起来拼团呀~",
+				    summary: "一起来拼图，描述summary",
+				    imageUrl: "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/uni@2x.png",
+				    success: function (res) {
+				        console.log("success:" + JSON.stringify(res));
+				    },
+				    fail: function (err) {
+				        console.log("fail:" + JSON.stringify(err));
+				    }
+				});
 			}
 		},
-		components:{
+		components: {
 			oneShopDetail
 		},
-		computed:{
-			
+		computed: {
+
 		},
 		onLoad(options) {
 			this.type = options.type
@@ -109,18 +123,20 @@
 </script>
 
 <style lang="less" scoped>
-	.container{
-		background:rgba(246,246,246,1);
+	.container {
+		background: rgba(246, 246, 246, 1);
 		padding-top: 20rpx;
-		.shopDetail{
+
+		.shopDetail {
 			width: 750rpx;
 			height: 240rpx;
 			padding: 0 30rpx;
-			background:#FFFFFF;
+			background: #FFFFFF;
 		}
+
 		// 拼团成功
-		.spellGroupSuccessCon{
-			.groupPeople{
+		.spellGroupSuccessCon {
+			.groupPeople {
 				width: 750rpx;
 				height: 224rpx;
 				display: flex;
@@ -128,110 +144,126 @@
 				align-items: center;
 				justify-content: center;
 				background: #FFFFFF;
-				.groupping{
+
+				.groupping {
 					text-align: center;
-					.countDown{
+
+					.countDown {
 						margin-bottom: 44rpx;
 					}
-					.shortPeople{
-						font-size:32rpx;
-						font-family:PingFang SC;
-						font-weight:bold;
-						line-height:38rpx;
-						color:rgba(51,51,51,1);
-						.num{
+
+					.shortPeople {
+						font-size: 32rpx;
+						font-family: PingFang SC;
+						font-weight: bold;
+						line-height: 38rpx;
+						color: rgba(51, 51, 51, 1);
+
+						.num {
 							color: #FF2F2F;
 							display: inline;
 						}
 					}
 				}
 			}
-			.groupSuccess{
+
+			.groupSuccess {
 				width: 750rpx;
 				height: 224rpx;
-				font-size:40rpx;
-				background:#FFFFFF;
-				font-family:PingFang SC;
-				font-weight:bold;
-				line-height:224rpx;
-				color:rgba(51,51,51,1);
+				font-size: 40rpx;
+				background: #FFFFFF;
+				font-family: PingFang SC;
+				font-weight: bold;
+				line-height: 224rpx;
+				color: rgba(51, 51, 51, 1);
 				text-align: center;
 			}
-			.groupUser{
+
+			.groupUser {
 				width: 750rpx;
 				height: 202rpx;
 				display: flex;
 				align-items: center;
 				justify-content: center;
 				background: #FFFFFF;
-				.oneGroupUser{
+
+				.oneGroupUser {
 					width: 110rpx;
 					display: flex;
 					flex-direction: column;
 					align-items: center;
 					justify-content: center;
 					margin: 0 35rpx;
-					.userImg{
+
+					.userImg {
 						margin-bottom: 12rpx;
-						image{
+
+						image {
 							width: 98rpx;
 							height: 98rpx;
 							border-radius: 50%;
 						}
 					}
-					.userName{
+
+					.userName {
 						width: 108rpx;
 						height: 32rpx;
-						font-size:28rpx;
-						font-family:PingFang SC;
-						font-weight:400;
-						line-height:32rpx;
-						color:rgba(51,51,51,1);
+						font-size: 28rpx;
+						font-family: PingFang SC;
+						font-weight: 400;
+						line-height: 32rpx;
+						color: rgba(51, 51, 51, 1);
 						overflow: hidden;
 						white-space: nowrap;
 						text-overflow: ellipsis;
-						
+
 					}
 				}
 			}
-			.inviteFriends{
+
+			.inviteFriends {
 				width: 750rpx;
 				height: 82rpx;
 				background: #FFFFFF;
 				padding-top: 20rpx;
-				.inviteBtn{
-					background:rgba(36,167,255,1);
+
+				.inviteBtn {
+					background: rgba(36, 167, 255, 1);
 					margin: 0 30rpx;
 					width: 690rpx;
 					height: 82rpx;
-					font-size:32rpx;
-					font-family:PingFang SC;
-					font-weight:bold;
-					line-height:82rpx;
-					color:rgba(255,255,255,1);
+					font-size: 32rpx;
+					font-family: PingFang SC;
+					font-weight: bold;
+					line-height: 82rpx;
+					color: rgba(255, 255, 255, 1);
 					text-align: center;
 					border-radius: 12rpx;
 				}
 			}
 		}
+
 		// 拼团失败
-		.spellGroupFailCon{
+		.spellGroupFailCon {
 			background: #FFFFFF;
 			padding: 0 30rpx;
-			.orderType{
+
+			.orderType {
 				width: 690rpx;
 				height: 68rpx;
 				display: flex;
 				justify-content: space-between;
 				align-items: center;
-				font-size:28rpx;
-				font-family:PingFang SC;
-				font-weight:400;
-				.text{
-					color:rgba(133,133,133,1);
+				font-size: 28rpx;
+				font-family: PingFang SC;
+				font-weight: 400;
+
+				.text {
+					color: rgba(133, 133, 133, 1);
 				}
-				.type{
-					color:rgba(51,51,51,1);
+
+				.type {
+					color: rgba(51, 51, 51, 1);
 				}
 			}
 		}
